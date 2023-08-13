8704d8c228c1d9b618558170e06282e12b2d113c
jerdson741 <jerdson.rec@gmail.com>
2023-08-13 20:44:32 -0300

    Create ex0003.py

---
+++ ex0003.py
@@ -0,0 +1,6 @@
+#crie um programa que faça a soma de 2 numeros
+n1 = int(input('insira um numero:'))
+n2 = int(input('insira outro numero:'))
+soma = n1+n2
+print(soma)
+print('a soma entre {} e {} é igual a {}!'.format(n1, n2,soma))
